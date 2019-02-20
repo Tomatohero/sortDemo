332b04d9b61832298b3f3a5a33621684140152b9
tomato <2457817475@qq.com>
2019-02-20 10:35:29 +0800

    Signed-off-by: tomato <2457817475@qq.com>

---
+++ git_idea04/src/main/java/com/example/demo/githjk.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class githjk {
+    private String getuser(){
+        return null;
+    }
+
+    public static void mian(String[] args){
+        System.out.println("再次尝试");
+    }
+}
